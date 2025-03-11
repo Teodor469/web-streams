5573906a3b7f36965abec97733f98425413ed8c9
Teodor469 <lifetroll6@gmail.com>
2025-03-11 14:17:36 +0200

    fixed the controller so the index method could be returned even if the user is not authorized

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('streams', StreamApiController::class)->middleware('auth:sanctum');
+Route::apiResource('streams', StreamApiController::class)
+    ->middleware('auth:sanctum')
+    ->except(['index']);
+
+Route::get('/streams', [StreamApiController::class, 'index'])->name('streams.index');
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
